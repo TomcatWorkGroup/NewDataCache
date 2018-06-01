936a3b9f1727ae2a79b3490e88380a4a608d9c2a
jialiang.li <80201288@qq.com>
2018-06-01 11:47:12 +0800

    将Device数据缓存Key由二十位变更位后十位

--- src/main/java/com/itdreamworks/newdatacache/controller/DeviceController.java
+++ src/main/java/com/itdreamworks/newdatacache/controller/DeviceController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+@RestController
 @RequestMapping("/device")
 public class DeviceController {
 
