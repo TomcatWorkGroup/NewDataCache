1a8dd477050ab140acaf805f66fcf91865ee9044
jialiang.li <80201288@qq.com>
2018-08-14 11:21:09 +0800

    删除无效配置及包导入

--- src/main/java/com/itdreamworks/newdatacache/receiver/NjrtT2RunByteMsgReceiver.java
+++ src/main/java/com/itdreamworks/newdatacache/receiver/NjrtT2RunByteMsgReceiver.java
@@ -4,16 +4,13 @@ import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.math.BigInteger;
-
-
 @Component
 public class NjrtT2RunByteMsgReceiver extends BaseReceiver {
 
     @Autowired
     private com.itdreamworks.newdatacache.utils.CacheUtil cacheUtil;
 
-    
+
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue("ByteNjrtMsgQueue"),
             exchange = @Exchange(EXCHANGE_NAME),
