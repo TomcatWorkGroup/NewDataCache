117ce1b6d5727a30e4b00911194075efc013b0a6
jialiang.li <80201288@qq.com>
2018-08-14 11:07:19 +0800

    将Cache Key从10或20个字符变更为仅10个字符长度

--- src/main/java/com/itdreamworks/newdatacache/utils/CacheUtil.java
+++ src/main/java/com/itdreamworks/newdatacache/utils/CacheUtil.java
@@ -10,35 +10,19 @@ import java.util.List;
 public class CacheUtil {
     @Autowired
     private ByteCacheUtil byteCacheUtil;
-    @Autowired
-    private Byte2CacheUtil byte2CacheUtil;
 
     public byte[] getCacheData(String key) {
-        if (key.length() == 10) {
-            return byteCacheUtil.getCacheData(key);
-        } else if (key.length() == 20) {
-            return byte2CacheUtil.getCacheData(key);
-        }
-        return null;
+        return byteCacheUtil.getCacheData(key);
     }
 
     public void putData(String key, byte[] data) {
-        if(key.length() == 10){
-            byteCacheUtil.putData(key, data);
-        }else if(key.length() == 20){
-            byte2CacheUtil.putData(key, data);
-            byteCacheUtil.putData(key.substring(10), data);
-        }
+        byteCacheUtil.putData(key, data);
     }
 
     public List<byte[]> getCacheDatas(String[] keys) {
         ArrayList<byte[]> ls = new ArrayList<>(keys.length);
         if (keys.length > 0) {
-            if (keys[0].length() == 10) {
-                return byteCacheUtil.getCacheDatas(keys);
-            } else if (keys[0].length() == 20) {
-                return byte2CacheUtil.getCacheDatas(keys);
-            }
+            return byteCacheUtil.getCacheDatas(keys);
         }
         return ls;
     }
